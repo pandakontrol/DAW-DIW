bb8416ab8566000bdc80a595659cbfd40e2798bd
pandakontrol <kevinkontrol97@gmail.com>
2019-10-22 16:03:35 +0200

    Arreglado bug mapa, huellas en cada direccion añadidas

--- UD02/Uncthuled/main.js
+++ UD02/Uncthuled/main.js
@@ -3,27 +3,28 @@
 var personajeX = 0;
 var personajeY = 5;
 
-mapa =[[1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-[0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0]];
+mapa =[[1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]];
 
 function listo(){
 
 document.querySelector(".mapa").innerHTML="";
 
-  for (var i=0;i<14;i++){
-    for (var j=0;j<21;j++){
+  for (var i=0;i<15;i++){
+    for (var j=0;j<23;j++){
       var newDiv = document.createElement("div");
       if (mapa[i][j]==0){
         newDiv.classList.add("camino");
@@ -36,7 +37,16 @@ document.querySelector(".mapa").innerHTML="";
         newDiv.classList.add("personaje");
       }
       else if (mapa[i][j]==4){
-        newDiv.classList.add("huellas");
+        newDiv.classList.add("huellasArriba");
+      }
+      else if (mapa[i][j]==5){
+        newDiv.classList.add("huellasAbajo");
+      }
+      else if (mapa[i][j]==6){
+        newDiv.classList.add("huellasIzquierda");
+      }
+      else if (mapa[i][j]==7){
+        newDiv.classList.add("huellasDerecha");
       }
       document.querySelector(".mapa").appendChild(newDiv);
     }
@@ -53,36 +63,50 @@ window.onload=function(){
 
 function moverAbajo(){
   console.log("Tecla abajo");
-  /* El número 4 pertenece a las huellas que deja el personaje */
-  mapa[personajeX][personajeY]=4;
-  personajeX++;
-  mapa[personajeX][personajeY]=2;
-  listo();
+  if (mapa[personajeX+1][personajeY] != 1) {
+    /* El número 5 pertenece a las huellasAbajo que deja el personaje */
+    mapa[personajeX][personajeY]=5;
+    personajeX++;
+    mapa[personajeX][personajeY]=2;
+    listo();
+  }
+
 }
 
 function moverArriba(){
   console.log("Tecla arriba");
-  /* El número 4 pertenece a las huellas que deja el personaje */
-  mapa[personajeX][personajeY]=4;
-  personajeX--;
-  mapa[personajeX][personajeY]=2;
-  listo();
+  if (mapa[personajeX-1][personajeY] != 1) {
+    /* El número 4 pertenece a las huellasArriba que deja el personaje */
+    mapa[personajeX][personajeY]=4;
+    personajeX--;
+    mapa[personajeX][personajeY]=2;
+    listo();
+  }
+
 }
 function moverIzquierda(){
   console.log("Tecla izquierda");
-  /* El número 4 pertenece a las huellas que deja el personaje */
-  mapa[personajeX][personajeY]=4;
-  personajeY--;
-  mapa[personajeX][personajeY]=2;
-  listo();
+  if (mapa[personajeX][personajeY-1] != 1) {
+    /* El número 6 pertenece a las huellasIzquierda que deja el personaje */
+    mapa[personajeX][personajeY]=6;
+    personajeY--;
+    mapa[personajeX][personajeY]=2;
+    listo();
+  }
+
 }
 function moverDerecha(){
   console.log("Tecla derecha");
-  /* El número 4 pertenece a las huellas que deja el personaje */
-  mapa[personajeX][personajeY]=4;
-  personajeY++;
-  mapa[personajeX][personajeY]=2;
-  listo();
+  // La condicion IF es para que cuando vea un 1 en el mapa( bloque) no avance
+  if (mapa[personajeX][personajeY+1] != 1) {
+    console.log(personajeY);
+    /* El número 7 pertenece a las huellasDerecha que deja el personaje */
+    mapa[personajeX][personajeY]=7;
+    personajeY++;
+    mapa[personajeX][personajeY]=2;
+    listo();
+  }
+
 }
 
 
