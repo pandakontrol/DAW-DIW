83cab25a76c9865456aafde3cccd3180b1fbf88d
pandakontrol <kevinkontrol97@gmail.com>
2019-11-03 17:18:55 +0100

    Me falta la animacion de volver, preguntar como hacer forEach a Angel

--- UD02/Tarea09/drumkit.js
+++ UD02/Tarea09/drumkit.js
@@ -1,15 +1,19 @@
-var key = document.querySelectorAll('.key');
+/* JINX DRUMKIT JAVASCRIPT! */
+
 
 /* Funcion para que inicie el audio */
 
-
 function playSound(e){
-
+  const keys = document.querySelectorAll('.key');
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const keyPressed = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+
   audio.play();
   audio.currentTime=0;
+/* Una vez presionemos las teclas que ya tenemos asignadas con data-key añadimos la clase transition */
+  keyPressed.classList.add('transition');
+
 
 }
 
@@ -18,6 +22,5 @@ function playSound(e){
 function removeTransition() {
   this.classList.remove('transition');
 }
-
-.key
-window.addEventListener('keydown', playSound)
+/* Añadimos un evento a toda la ventana, que cuando pulsemos la tecla ejecute la función llamada */
+window.addEventListener('keydown', playSound);
