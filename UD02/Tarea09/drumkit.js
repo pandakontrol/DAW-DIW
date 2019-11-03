1d62b1a61ea0f8c7baa17f80c9563041336c8546
pandakontrol <kevinkontrol97@gmail.com>
2019-11-03 15:02:17 +0100

    Sigue faltando foreach

--- UD02/Tarea09/drumkit.js
+++ UD02/Tarea09/drumkit.js
@@ -1,6 +1,23 @@
+var key = document.querySelectorAll('.key');
+
+/* Funcion para que inicie el audio */
+
 
 function playSound(e){
 
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keyPressed = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  audio.play();
+  audio.currentTime=0;
 
 }
+
+/* Funcion que borra la transición creada */
+
+function removeTransition() {
+  this.classList.remove('transition');
+}
+
+.key
+window.addEventListener('keydown', playSound)
