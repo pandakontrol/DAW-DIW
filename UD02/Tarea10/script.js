9ec44dd3131d44d1a4628328bfecb554e394acef
pandakontrol <kevinkontrol97@gmail.com>
2019-11-05 18:06:59 +0100

    Terminada

--- UD02/Tarea10/script.js
+++ UD02/Tarea10/script.js
@@ -1,1 +1,15 @@
-document.querySelector
+
+
+function mover() {
+  document.querySelector('button').addEventListener('click',transicion);
+}
+
+function transicion() {
+
+ let cajas = document.querySelectorAll('.caja');
+
+ cajas.forEach(cajas => cajas.classList.toggle('transition'));
+
+}
+
+mover();
