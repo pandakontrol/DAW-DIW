9980325273ac5879408cc3a2a42e3bacfbeb5488
pandakontrol <kevinkontrol97@gmail.com>
2019-11-07 20:03:41 +0100

    Añadiendo cajas verdes

--- UD02/Tarea12/script.js
+++ UD02/Tarea12/script.js
@@ -1,7 +1,16 @@
-/* 
- 
-    ^(;,;)^ : Fragmento perdido
 
-*/
 
 window.onload=init;
+
+function init() {
+
+  document.querySelector('button').addEventListener('click', crearCaja);
+
+}
+
+function crearCaja(){
+
+  let box = document.createElement('box');
+  document.querySelector('container').appendChild(box);
+
+}
