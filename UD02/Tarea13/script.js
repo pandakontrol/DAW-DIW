89f09bbe8a173d009267911383c2f714a575d0fa
pandakontrol <kevinkontrol97@gmail.com>
2019-11-10 04:36:23 +0100

    Haciendo pruebas ejercicio 13

---
+++ UD02/Tarea13/script.js
@@ -0,0 +1,48 @@
+
+
+window.onload=init;
+
+function init() {
+
+  document.querySelector('button').addEventListener('click', crearCaja);
+
+}
+/* Funcion que añade las cajas y llama añade un evento al hacer click */
+
+function crearCaja(){
+  contador= document.querySelectorAll('box').length;
+  let box = document.createElement('box');
+  if (contador < 20) {
+    document.querySelector('container').appendChild(box);
+    box.addEventListener('click',evolucion);
+  }
+
+}
+
+/* Primera evolucion, añade la clase al ultimo evento */
+
+function evolucion(){
+this.classList.add('evoluciona');
+this.addEventListener('click', desevolucion);
+
+}
+/* Convierte el cuadrado en azul */
+
+function desevolucion(){
+  this.classList.replace('evoluciona','desevoluciona');
+  this.addEventListener('click',ultimate);
+}
+/* Evoluciona a cthulhu */
+
+function ultimate(){
+  this.classList.replace('desevoluciona','ultimate');
+  this.addEventListener('click',rotate)
+}
+
+/* Funcion para que roten los cthulhu */
+
+function rotate(){
+this.classList.replace('ultimate','rotate');
+
+}
+/* Funcion para que giren los cthulhu */
