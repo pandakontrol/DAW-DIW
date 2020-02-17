fe1a161624142f1f27a7b1b0e0c76ccf51ff812f
pandakontrol <kevinkontrol97@gmail.com>
2020-02-17 16:54:26 +0100

    Filtro pokemon

--- PokeDex/public/js/script.js
+++ PokeDex/public/js/script.js
@@ -11,52 +11,44 @@ const pokemonUrl = "https://pokeapi.co/api/v2/pokemon";
 
 // Esta es la funcion de filtrado de pokemons
 
-/*function filtroLetra(elemento) {
-    let letra = document.querySelector(`input[name="nombre"]`).value;
-    return elemento.properties.nombre.startsWith(letra);
-}*/
+function filtroLetra(elemento) {
+    let letra = document.querySelector(`input[name="buscar"]`).value;
+    return elemento.name.startsWith(letra);
+}
 
 function buscar() {
-    // Obtenemos el JSON que esta definido
+
     const fetchPromesa = fetch(pokemonUrl);
 
-    // Cuando se resuelva la promesa
     fetchPromesa
         .then(response => {
-            // la pasamos a JSON
+            // Pasamos la promesa a JSON
             return response.json();
-
-            // Y entonces
         })
         .then(respuesta => {
-            // Filtramos los resultados con el filtro definido anteriormente
-            const resultado = respuesta.name;
-
+            //Filtramos los resultados con el filtro anterior
+            const resultado = respuesta.results.filter(filtroLetra);
+            console.log(resultado);
             // Una vez tenemos el listado filtrado pasamos a crear
 
-            let listado = document.createElement("div");
-            listado.classList.add("resultados2");
+            //  let listado = document.createElement("div");
+            //listado.classList.add("resultados2");
             // Por cada uno de ellos
-            resultado.forEach(pokemon => {
+            resultado.forEach(falla => {
 
-                let pokemonDiv = document.createElement("div");
-                let ubiButton = document.createElement("button");
-                ubiButton.classList.add("ubiButton");
-                ubiButton.innerHTML = "Ubicacion";
-                pokemonDiv.classList.add("pokemon");
-
-                pokemonDiv.innerHTML = "<img src=" + pokemon.properties.boceto + ">" + pokemon.properties.nombre;
-
-                pokemonDiv.appendChild(ubiButton);
+                // let fallaDiv = document.createElement("div");
+                // fallaDiv.classList.add("falla");
+                // fallaDiv.innerHTML = "<img src=" + falla.properties.boceto + ">" + falla.properties.nombre;
                 // Lo anyadimos
-                listado.appendChild(pokemonDiv);
+                //  listado.appendChild(fallaDiv);
 
             });
             // Establecemos el listado en la Web
-            document.querySelector(".resultados").innerHTML = "";
-            document.querySelector(".resultados").appendChild(listado);
+            //document.querySelector(".resultados").innerHTML = "";
+            // document.querySelector(".resultados").appendChild(listado);
         });
 }
+
 
 function init() {
 
